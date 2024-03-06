38d4f03077949eb241d71d82d8836abb28f1787f
Oleg <obadmazabe@gmail.com>
2024-03-06 20:38:48 +0200

    create index.html + index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+let myYearsOfBrith = 2001;
+
+console.log(myYearsOfBrith);
+
+const MY_AGE = 22;
+
+console.log(MY_AGE);
+
+let Myname = "Oleg";
+let name2 ='Badmazhabe';
+let bio = `
+Привет, меня зовут ${Myname}!
+Моя фамилия ${name2}
+Я родом из России, Сибири
+Мой возраст ${2024 - myYearsOfBrith}
+`;
+
+console.log(bio);
+
